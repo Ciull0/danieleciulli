48ecd3d5863ad1ee144a4f132637fa71f0868042
Daniele Ciulli <d.ciulli@sintraconsulting.eu>
2021-05-11 15:49:02 +0200

    Restart From Skatch with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { JoblistComponent } from './joblist/joblist.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TextboxComponent } from './textbox/textbox.component';
+import { HeroComponent } from './hero/hero.component';
+import { CardComponent } from './card/card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    JoblistComponent,
+    ProjectsComponent,
+    TextboxComponent,
+    HeroComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
