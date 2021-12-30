759f587f8278a536d30ebe328c80c9e0f5da165b
Daniele Ciulli <d.ciulli@sintraconsulting.eu>
2021-12-31 00:00:41 +0100

    Navigation, core structure

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
+  textTest: string = 'Hello World';
+  content: Array<string> = [];
 
   ngOnInit(): void {
+    for(let i=0; i<200; i++){
+      this.content.push(this.textTest);
+    }
   }
 
 }
